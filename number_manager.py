7ab34f41680f5a01220b748c3fb35c1645c97c90
rorypearson26 <rorypearson2605@gmail.com>
2020-07-02 18:35:53 +0100

    Change to OOP

--- number_manager.py
+++ number_manager.py
@@ -1,29 +1,46 @@
 import mysql.connector
 
-def database_connection():
-    """Function to open connection with the document_manager database"""
-    mydb = mysql.connector.connect(
-        host='localhost',
-        user='doc_manager',
-        passwd='GetMeDocNumbers789',
-        database='test_doc_db'
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute('SELECT * FROM doc_names')
-    myresult = mycursor.fetchall()
-    print(myresult)
+class MyDocName:
+    length = 4
+    def __init__(self, project_number, doc_type, doc_number):
+        self.project_number = project_number
+        self.doc_type = doc_type
+        self.doc_number = doc_number
 
+    def name_formatter(self):
+        """Function to format number together"""
+        project_number = MyDocName.number_padder(self.project_number, 4)
+        doc_number = MyDocName.number_padder(self.doc_number, 4)
+        doc_name = "".join([str(project_number), "-", str(self.doc_type), "-", str(doc_number)])
+        return doc_name
 
-def doc_name_maker(project, doc_type, doc_number):
-    """Function to construct the full document name"""
-    project = zero_padder(project, 4)
-    doc_number = zero_padder(doc_number, 4)
-    doc_name = "".join([str(project), "-", str(doc_type), "-", str(doc_number)])
-    return doc_name
+    def number_padder(input_string, length):
+        """Function to pad out strings properly with leading zeros"""
+        return str(input_string).zfill(length)
 
-def zero_padder(input_str, length):
-    """Function to pad out strings properly with leading zeros"""
-    return str(input_str).zfill(length)
+class MyDB:
+    """Class to create and use a database object"""
+    def __init__(self, args):
+        """Create the attributes needed for connection to the database"""
+        self.host = args[0]
+        self.user = args[1]
+        self.password = args[2]
+        self.db_name = args[3]
 
-#print(doc_name_maker(1, "DP", 1))
-database_connection()
+    def connect(self):
+        """Function to establish connection with the database"""
+        mydb = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            passwd=self.password,
+            database=self.db_name
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute('SELECT * FROM doc_names')
+        myresult = mycursor.fetchall()
+        print(myresult)
+
+db = MyDB(['localhost', 'doc_manager', 'GetMeDocNumbers789', 'test_doc_db'])
+my_doc = MyDocName(1, 'dp', 1)
+db.connect()
+print(my_doc.name_formatter())
