c4ecad56db321ccacbdd413c6ba577b79bf705c3
rorypearson26 <rorypearson2605@gmail.com>
2020-07-21 15:48:12 +0100

    Create new number form mods

--- app/document_app.py
+++ app/document_app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://doc_manager:GetMyDocNumbers789@localhost/DocumentNumbers'
+app.config['SECRET_KEY'] = '135d473ff2af445988f0a4be01a5b77c'
 db = SQLAlchemy(app)
 
 class Project(db.Model):
@@ -30,13 +35,14 @@ class DocumentNumber(db.Model):
 
 @app.route('/', methods=["POST", "GET"])
 def home():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("projects", test_content=user))
+    form = CreateNumberForm()
+    if form.validate_on_submit():
+        flash(f"Document number created", "success")
+        return redirect(url_for('home'))
     else:
-        return render_template("index.html", proj_vals=Project.query.all(),
-                               doc_type_vals=DocumentType.query.all(),
-                               content="This is the homepage")
+        flash_errors(form)
+        return render_template("index.html", form=form)
+
 @app.route('/projects')
 def projects():
     return render_template("projects.html")
@@ -44,6 +50,38 @@ def projects():
 @app.route('/doctypes')
 def doc_types():
     return render_template("documenttypes.html", content="Here are the doc types")
+
+def project_query():
+    return Project.query.order_by(Project.id)
+
+def doc_type_query():
+    return DocumentType.query.order_by(DocumentType.id)
+
+def flash_errors(form):
+    """Flashes form errors and displayed as warning"""
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error), 'warning')
+
+def doc_name_maker(Document):
+    """Function to construct the full document name"""
+    project = zero_padder(Document.project, 4)
+    doc_number = zero_padder(Document.doc_number, 4)
+    Document.doc_name = "".join([str(project), "-", str(Document.doc_type), "-", str(doc_number)])
+    return Document
+
+class CreateNumberForm(FlaskForm):
+    """Define fields required for creating a new number form"""
+
+    description = StringField(label='Description',
+                              render_kw={"placeholder": "Fill out sensible name",
+                                         "maxlength": 40},
+                              validators=[DataRequired(), Length(min=10, max=40)])
+    project = QuerySelectField(query_factory=project_query, get_label="name")
+    doc_type = QuerySelectField(query_factory=doc_type_query, get_label="initials")
+    submit = SubmitField('Create')
 
 if __name__ == "__main__":
     db.create_all()
